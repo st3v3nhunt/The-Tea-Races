63a573e19d7c0aca0447a19f1b653c2b71346a09
steven hunt <st3v3n.hunt@gmail.com>
2011-05-01 15:03:37 +0100

    Dynamically sized canvas and race duration.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
+(function () {
 function Page () {
 	var self = this,
 	 $canvas = $('#canvas')[0],
 	$context = $canvas.getContext('2d');
 	
+	this.setCanvasSize = function () {
+		height = $(document).height();
+		width = $(document).width();
+		$canvas.height = height-2;
+		$canvas.width = width-2;
+	};
+	
 	this.getContext = function () {
 		return $context;
-	};
-
-	this.draw = function (x) {
-		var context = $context;
-		context.fillRect(100,100,100,100);
 	};
 
 	this.clearCanvas = function () {
@@ -20,28 +23,32 @@ function Page () {
 
 };
 
-function Car (context, x, y) {
+function Car (context, name, x, y) {
 	var self = this,
 	 timerId = 0,
 			 ctx = context,
 		xStart = x,
-		yStart = y,
-				 x = x,
-				 y = y;
+		yStart = y;
+
+	this.x = x;
+	this.y = y;
+	this.name = name;
+
+	this.getDistance = function () { return this.x; };
 
 	this.drawCar = function () {
-		ctx.fillRect(x,y,10,10);
+		ctx.fillRect(this.x,this.y,10,10);
 	};
 
 	this.clearCar = function () {
-		ctx.clearRect(xStart,yStart,10+x,10+y);
+		ctx.clearRect(xStart,yStart,10+this.x,10+y);
 	};
 
 	this.start = function () {
 		timerId = setInterval( function () {
 								self.clearCar();
 								self.drawCar();
-								x = x+Math.ceil(Math.random()*2);
+								self.x = self.x+Math.floor(Math.random()*3+1);
 							}, 10);
 	};
 	
@@ -53,42 +60,55 @@ function Car (context, x, y) {
 
 function Race () {
 	var self = this,
-	entrants = [];
+	entrants = [],
+	duration = ($(document).width() - 100 / 2) * 5;
 
-	this.register = function (car) {
-		entrants.push(car);
+	this.registerCars = function (cars) {
+		for (var i=0; i<cars.length; i++) {
+			entrants.push(cars[i]);
+		}
 	};
 
 	this.start = function () {
-		var i = 0;
-		for (i=0;i < entrants.length; i++) {
+		for (var i=0;i < entrants.length; i++) {
 			entrants[i].start();
+		}
+
+		setTimeout(stop, duration);
+	};
+	
+	declareWinnner = function () {
+		var winner = { name: '', distance: 0 };
+		for (var i=0;i < entrants.length; i++) {
+			var entrantDistance = entrants[i].getDistance();
+			if (entrantDistance > winner.distance) {
+				winner.distance = entrantDistance;
+				winner.name = entrants[i].name;
+			}
 		}
 	};
 
-	this.stop = function () {
-		var i = 0;
-		for (i=0;i < entrants.length; i++) {
+	stop = function () {
+		for (var i=0;i < entrants.length; i++) {
 			entrants[i].stop();
 		}
+		declareWinnner();
 	};
 };
 
 $(document).ready(function () {
 	var page = new Page();
-	var car = new Car(page.getContext(), 50, 50);
-	var car2 = new Car(page.getContext(), 50, 150);
-	var car3 = new Car(page.getContext(), 50, 250);
-	
-	//car.start();
-	
-	var race = new Race();
-	race.register(car);
-	race.register(car2);
-	race.register(car3);
-	
-	race.start();
-	setTimeout(race.stop, 6000);
+	page.setCanvasSize();
 
+	var car = new Car(page.getContext(), 'me', 50, 50);
+	var car2 = new Car(page.getContext(), 'you', 50, 150);
+	var car3 = new Car(page.getContext(), 'them', 50, 250);
+	var cars = [car, car2, car3];
+
+	var race = new Race();
+	race.registerCars(cars);
+
+	race.start();
 
 });
+})();
